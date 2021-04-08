f7f7f0c1737e4a285153780bda223c085a786a59
beautidata <beautifuldatadev@gmail.com>
2021-04-08 14:13:52 -0500

    add 404 not found and ifmain

--- app.py
+++ app.py
@@ -1,7 +1,15 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, make_response
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/<page_name>')
+def other_page(page_name):
+    response = make_response('The page named %s does not exist.' % page_name, 404)
+    return response
+
+if __name__ == '__main__':
+    app.run(debug=True)
