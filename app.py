6e4cfe99f101ea513bf85e4de5031db2a8ceeb47
beautidata <beautifuldatadev@gmail.com>
2021-04-10 21:21:38 -0500

    add ability to save new posts

--- app.py
+++ app.py
@@ -40,6 +40,20 @@ def page_not_found(page_name):
 
 @app.route('/create', methods=('GET', 'POST'))
 def create():
+    if request.method == 'POST':
+        title = request.form['title']
+        content = request.form['content']
+
+        if not title:
+            flash('Title is required!')
+        else:
+            conn = get_db_connection()
+            conn.execute('INSERT INTO posts (title, content) VALUES (?, ?)', (title, content))
+            conn.commit()
+            conn.close()
+            return redirect(url_for('index'))
+
+        
     return render_template('create.html')
 
 if __name__ == '__main__':
