9b2c0f316f69c1d33cb52f06813abcfe5248fa67
beautidata <beautifuldatadev@gmail.com>
2021-04-26 17:52:14 -0500

    add google login wip

--- app.py
+++ app.py
@@ -1,7 +1,20 @@
-from flask import Flask, render_template, make_response, request, url_for, flash, redirect
 import sqlite3
+
+from flask import Flask, render_template, make_response, request, url_for, flash, redirect
+from flask_login import (
+    LoginManager,
+    current_user,
+    login_required,
+    login_user,
+    logout_user
+)
+from oauthlib.oauth2 import WebApplicationClient
+import requests
 from werkzeug.exceptions import abort
 from configparser import ConfigParser
+
+from db import init_db_command
+from user import User
 
 def get_db_connection():
     conn = sqlite3.connect('database.db')
@@ -16,17 +29,90 @@ def get_post(post_id):
         abort(404)
     return post
 
+def get_google_provider_cfg():
+    return requests.get(GOOGLE_DISCOVERY_URL).json()
+
 app = Flask(__name__)
 config = ConfigParser()
 config.readfp(open('settings.ini'))
 app.config['SECRET_KEY'] = config.get('flaskalicious', 'secret_key')
+GOOGLE_CLIENT_ID = config.get('google', 'client_id')
+GOOGLE_CLIENT_SECRET = config.get('google', 'client_secret')
+GOOGLE_DISCOVERY_URL = (
+    "https://accounts.google.com/.well-known/openid-configuration"
+)
+
+login_manager = LoginManager()
+login_manager.init_app(app)
+
+client = WebApplicationClient(GOOGLE_CLIENT_ID)
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.get(user_id)
 
 @app.route('/')
 def index():
-    conn = get_db_connection()
-    posts = conn.execute('SELECT * FROM posts').fetchall()
-    conn.close()
-    return render_template('index.html', posts=posts)
+    if current_user.is_authenticated:
+        conn = get_db_connection()
+        posts = conn.execute('SELECT * FROM posts').fetchall()
+        conn.close()
+        return render_template('index.html', posts=posts)
+    else:
+        return '<a class="button" href="/login">Google Login</a>'
+
+@app.route('/login')
+def login():
+    google_provider_cfg = get_google_provider_cfg()
+    authorization_endpoint = google_provider_cfg['authorization_endpoint']
+
+    request_uri = client.prepare_request_uri(
+        authorization_endpoint,
+        redirect_uri=request.base_url + '/callback',
+        scope=['openid', 'email', 'profile'],
+    )
+    return redirect(request_uri)
+
+@app.route('/login/callback')
+def callback():
+    code = request.args.get('code')
+    google_provider_cfg = get_google_provider_cfg()
+    token_endpoint = google_provider_cfg['token_endpoint']
+    token_url, headers, body = client.prepare_token_request(
+        token_endpoint,
+        authorization_response=request.url,
+        redirect_url=request.base_url,
+        code=code
+    )
+    token_response = requests.post(
+        token_url,
+        headers=headers,
+        data=body,
+        auth=(GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET),
+    )
+    client.parse_request_body_response(json.dumps(token_response.json()))
+    userinfo_endpoint = google_provider_cfg['userinfo_endpoint']
+    uri, headers, body = client.add_token(userinfo_endpoint)
+    userinfo_response = requests.get(uri, headers=heeaders, data=body)
+    if userinfo_response.json().get('email_verified'):
+        unique_id = userinfo_response.json()["sub"]
+        users_email = userinfo_response.json()['sub']
+    else:
+        return "User email not available or not verified by Google.", 400
+    
+    user = User(id_=unique_id, email=users_email)
+
+    if not User.get(unique_id):
+        User.create(unique_id, users_name, users_email, picture)
+    
+    login_user(user)
+    return redirect(url_for('index'))
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
 
 @app.route('/<int:post_id>')
 def post(post_id):
@@ -87,4 +173,4 @@ def delete(id):
     return redirect(url_for('index')) 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, ssl_context='adhoc')
